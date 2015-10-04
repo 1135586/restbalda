6b7c47766e647a0b5a7c45bfc74e8125d375651e
dmitry <1135586@gmail.com>
2015-10-04 14:04:52 +0600

    xz

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import Scores
-#import models
+#from models import Scores
+import models
 
 tasks = [
     {
